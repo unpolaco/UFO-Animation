cb315f5f3ac1c7b17de46948d83cad009901f7d6
Michal Borowski <unpolacoo@gmail.com>
2019-09-12 01:19:45 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const animBg = new AnimBg('.anim')
+
+  document.addEventListener('mousemove', (event) => animBg.listenCursorMove(event))
+})
+
