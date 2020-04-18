90a067ec6b34ce095f99c571102e601947a868b4
Michal Borowski <unpolacoo@gmail.com>
2020-04-18 10:36:47 +0200

    Add screenshot, cleaning files

--- AnimBg.js
+++ AnimBg.js
@@ -1,30 +1,20 @@
 class AnimBg {
-  constructor(selector) {
-    this.elements = document.querySelectorAll(selector);
-  }
+	constructor(selector) {
+		this.elements = document.querySelectorAll(selector);
+	}
 
-  listenCursorMove(event) {
-    const { clientX, clientY } = event;
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
+	listenCursorMove(event) {
+		const { clientX, clientY } = event;
+		const centerX = window.innerWidth / 2;
+		const centerY = window.innerHeight / 2;
+		const posX = clientX - centerX;
+		const posY = clientY - centerY;
+		this.elements.forEach((el) => this.moveElement(el, posX, posY));
+	}
 
-    const posX = clientX - centerX;
-    const posY = clientY - centerY;
-
-    this.elements.forEach(el => this.moveElement(el, posX, posY));
-  }
-
-  moveElement(el, posX, posY) {
-    const ratioX = -el.getAttribute('ratioX');
-    const ratioY = -el.getAttribute('ratioY');
-
-    el.style.transform = `translate(${posX * ratioX}px, ${posY * ratioY}px)`;
-  }
+	moveElement(el, posX, posY) {
+		const ratioX = -el.getAttribute('ratioX');
+		const ratioY = -el.getAttribute('ratioY');
+		el.style.transform = `translate(${posX * ratioX}px, ${posY * ratioY}px)`;
+	}
 }
-
-// const Planet = function(x, y, speed, sprite) {
-//   this.x = x;
-//   this.y = y;
-//   this.speed = speed;
-//   this.sprite = 'images/ghost' + sprite + '.png';
-// };
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const animBg = new AnimBg('.anim')
-
   document.addEventListener('mousemove', (event) => animBg.listenCursorMove(event))
 })
 
